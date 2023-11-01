0f4c8d9a4c19c315228ac78216dfbbce42efb2e7
liliana0514 <148395601+liliana0514@users.noreply.github.com>
2023-11-01 21:09:04 +0000

    00

--- cli.py
+++ cli.py
@@ -1,1 +1,30 @@
+def get_empty_board():
+    return [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
 
+def print_board(board):
+  for row in board:
+    print(row)
+
+def get_player_input():
+    """
+    return:
+        row : int --> the index of row
+        col : int --> the index of colum
+    """
+    player_input = input(f">")
+    print(player_input)
+
+if __name__ == '__main__':
+  # get an empty board
+  board=get_empty_board()
+  #print the board
+  print_board(board)
+  # ask user input
+  row,col = get_player_input()
+  # check for winner
+  # check if game is draw
+  # print the winner
